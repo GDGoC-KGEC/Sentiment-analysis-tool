a68c88f2125029b63d36245afafd6eedef599564
Hriddhiman <hriddhiman2008@gmail.com>
2025-11-05 13:02:25 +0530

    Used CORS

--- model-lab/app/main.py
+++ model-lab/app/main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI, File, UploadFile
 from PIL import Image
 import io
 from app.model import predict_emotion
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Face Sentiment API")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
